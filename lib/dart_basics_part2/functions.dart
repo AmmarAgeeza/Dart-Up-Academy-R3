0a1288914589776a41e35544f10803c9b635dafa
AmmarAgeeza <ammarfathy516@gmail.com>
2024-03-13 00:00:14 +0200

    functions

--- lib/dart_basics_part2/functions.dart
+++ lib/dart_basics_part2/functions.dart
@@ -2,8 +2,8 @@ void main() {
   // String firstNameAbdallah = 'Abdallah';
   // String lastNameSamir = 'Samir';
   // String nickNameBoda = 'Boda';
-  // String AbdallahFullName = '$firstNameAbdallah $lastNameSamir $nickNameBoda';
-  // print(AbdallahFullName);
+  // String abdallahFullName = '$firstNameAbdallah, $lastNameSamir, $nickNameBoda';
+  // print(abdallahFullName);
 
   // String firstNameKareem = 'Kareem';
   // String lastNameAdel = 'Adel';
@@ -11,19 +11,126 @@ void main() {
   // String kareemFullName = '$firstNameKareem $lastNameAdel $nickNameKimo';
   // print(kareemFullName);
 
-  concteneName('Abdallah', 'Samir', 'Boda');
-  concteneName('Kareem', 'Adel', 'Kimo');
+  // concteneName('Abdallah', 'Samir', 'Boda');
+  // concteneName('Kareem', 'Adel', 'Kimo');
+  // printFullName();
+  // printFullNameWithInput('Firest', 'Lname');
+  // getFullName(); //return String
+  // 'HelloFlutter';
+  // String data = 'data';
+  // print(getFullName());
+  // String reult=getFullName();
+  // print(reult);
+  // print(getFullNameWithInput('Hello', 'Dart'));
+  // printName('Ali');
+  // printPrice(discount: 490, price: 500, total: 500);
+
+//  late int x;
+//  x=5;
+//   int? x;
+//   x=5;
+//  x=null; // nullable => your var can be either value or null.
+  // print(x!+1) ;
+//   if(x!=null){
+//     print(x+1) ;
+//   }else{
+//         print(0+1) ;
+
+//   }
+//   print(x??0+1) ;
+// printItemPrice(price: 250,discount: 25);
+// printItemPrice(price: 500,discount: 250);
+  printItemPriceWithDiscountDefault(price: 500, discount: 60);
+// int x=4;
+// int y=5;
+print(swap().num1);
+print(swap().num2);
+print(swap().sum);
 }
+//record
+({int num1, int num2,int sum}) swap() {
+  int x = 4;
+  int y = 5;
+  return (num1:y, num2:x,sum:x+y);
+}
+//search for Either in dartz package.
+//bug - error
+
+//TODO: searhc logical error.
 
 // void
 /*
 returnType funName (){
-
+body of function
 }
 
  */
 
-String concteneName(String fName, String lName, String nickName) {
-  print('FirstName:$fName, LastNAme:$lName, NickNAme:$nickName');
-  return '';
+// String concteneName() {
+//   print('FirstName:$fName, LastNAme:$lName, NickNAme:$nickName');
+//   return '';
+// }
+
+//different cases of function
+
+//!case 1
+void printFullName() {
+  String firstName = 'Hello';
+  String lastName = 'Flutter';
+  print(firstName + lastName);
+}
+
+//!case 2
+void printFullNameWithInput(String fname, String lname) {
+  print(fname + lname);
+}
+
+//!case 3
+String getFullName() {
+  String firstName = 'Hello';
+  String lastName = 'Flutter';
+  // String fullName=firstName+lastName;
+  return firstName + lastName;
+}
+
+//! case 4
+String getFullNameWithInput(String fname, String lname) {
+  return fname + lname;
+}
+
+// Types of Parameters
+
+//! 1.Required
+
+// void printName(String name) {
+//   print('Name=$name');
+// }
+
+//arrow function
+void printName(String name) => print('Name=$name');
+
+//! 2.named parameters
+void printPrice({
+  required int price,
+  required int discount,
+  required int total,
+}) {
+  print('$total= $price - $discount');
+}
+
+//! 3.optional parameters
+
+void printItemPrice({
+  required int price,
+  int? discount, //null
+}) {
+  print('Total = ${price - (discount ?? 0)}');
+}
+
+//! 4.Default Parameters
+void printItemPriceWithDiscountDefault({
+  required int price,
+  int discount = 50,
+}) {
+  print('Total = ${price - discount}');
 }
--- lib/dart_oop_part_1/dart_oop_part_1.dart
+++ lib/dart_oop_part_1/dart_oop_part_1.dart
@@ -1,3 +1,7 @@
+import '../dart_basics_part2/functions.dart';
+
 void main(){
   print('ss');
+  //task2
+  printItemPriceWithDiscountDefault(price: 500);
 }
